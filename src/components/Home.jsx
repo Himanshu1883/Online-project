5e0589e87c4e290cef6d61f114526a002f6a595e
Himanshu Dubey <himanshudubeuhd@gmail.com>
2025-03-20 16:07:21 +0530

    Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ function Home() {
   return (
     <>
       <h2>Home Page</h2>
+      <img src="https://images.pexels.com/photos/16057187/pexels-photo-16057187/free-photo-of-close-up-of-cellphones.jpeg?auto=compress&cs=tinysrgb&w=600" alt="home" className="w-50"/>
       </>
   )
 }
